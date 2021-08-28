754e55f34302e45a6514ba181f78c3f143a33855
prasunranjan35 <89660540+prasunranjan35@users.noreply.github.com>
2021-08-28 13:58:58 +0530

    Update Activity_03.py

--- Activity_03.py
+++ Activity_03.py
@@ -1,3 +1,19 @@
-first= "good"
-second = "morning"
+//for concatinating once.
+first= "Good"
+second = "Morning"
 print (first + second)
+
+//for concatinating five times.
+first= "Good"
+second = "Morning"
+print ((first + second)*5)
+
+//concatinating in single line with spaces.
+first= "Good"
+second = "Morning "
+print ((first + second)*5)
+
+// concatinating with /n.
+first= "Good"
+second = "Morning\n "
+print ((first + second)*5)
